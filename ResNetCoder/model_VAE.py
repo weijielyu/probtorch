2585ee6501a4368ffbe7ce142823bfb7fea018f0
Weijie <wlyu3@illinois.edu>
2022-05-06 00:02:26 -0500

    Change Encoder to ResNet18

---
+++ ResNetCoder/model_VAE.py
@@ -0,0 +1,118 @@
+# This file is modified from <https://github.com/julianstastny/VAE-ResNet18-PyTorch>
+
+import torch
+import torch.nn.functional as F
+from torch import nn
+import torchvision.models as models
+
+class ResizeConv2d(nn.Module):
+
+    def __init__(self, in_channels, out_channels, kernel_size, scale_factor, mode='nearest'):
+        super().__init__()
+        self.scale_factor = scale_factor
+        self.mode = mode
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=1)
+
+    def forward(self, x):
+        x = F.interpolate(x, scale_factor=self.scale_factor, mode=self.mode)
+        x = self.conv(x)
+        return x
+
+class ResNet18Enc(nn.Module):
+    def __init__(self, z_dim=20):
+        super().__init__()
+        self.z_dim = z_dim
+        self.ResNet18 = models.resnet18(pretrained=True)
+        self.num_feature = self.ResNet18.fc.in_features
+        self.ResNet18.fc = nn.Linear(self.num_feature, 2*self.z_dim)
+
+    def forward(self, x):
+        x = self.ResNet18(x)
+        mu = x[:,:self.z_dim]
+        logvar = x[:,self.z_dim:]
+        return mu, logvar
+
+
+class BasicBlockDec(nn.Module):
+
+    def __init__(self, in_planes, stride=1):
+        super().__init__()
+
+        planes = int(in_planes/stride)
+
+        self.conv2 = nn.Conv2d(in_planes, in_planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(in_planes)
+
+        if stride == 1:
+            self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+            self.bn1 = nn.BatchNorm2d(planes)
+            self.shortcut = nn.Sequential()
+        else:
+            self.conv1 = ResizeConv2d(in_planes, planes, kernel_size=3, scale_factor=stride)
+            self.bn1 = nn.BatchNorm2d(planes)
+            self.shortcut = nn.Sequential(
+                ResizeConv2d(in_planes, planes, kernel_size=3, scale_factor=stride),
+                nn.BatchNorm2d(planes)
+            )
+
+    def forward(self, x):
+        out = torch.relu(self.bn2(self.conv2(x)))
+        out = self.bn1(self.conv1(out))
+        out += self.shortcut(x)
+        out = torch.relu(out)
+        return out
+
+class ResNet18Dec(nn.Module):
+
+    def __init__(self, num_Blocks=[2,2,2,2], z_dim=20, nc=3):
+        super().__init__()
+        self.in_planes = 512
+
+        self.linear = nn.Linear(z_dim, 512)
+
+        self.layer4 = self._make_layer(BasicBlockDec, 256, num_Blocks[3], stride=2)
+        self.layer3 = self._make_layer(BasicBlockDec, 128, num_Blocks[2], stride=2)
+        self.layer2 = self._make_layer(BasicBlockDec, 64, num_Blocks[1], stride=2)
+        self.layer1 = self._make_layer(BasicBlockDec, 64, num_Blocks[0], stride=1)
+        self.conv1 = ResizeConv2d(64, nc, kernel_size=3, scale_factor=2)
+
+    def _make_layer(self, BasicBlockDec, planes, num_Blocks, stride):
+        strides = [stride] + [1]*(num_Blocks-1)
+        layers = []
+        for stride in reversed(strides):
+            layers += [BasicBlockDec(self.in_planes, stride)]
+        self.in_planes = planes
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.linear(x)
+        x = x.view(x.size(0), 512, 1, 1)
+        x = F.interpolate(x, scale_factor=7)
+        x = self.layer4(x)
+        x = self.layer3(x)
+        x = self.layer2(x)
+        x = self.layer1(x)
+        x = F.interpolate(x, size=(112, 112), mode='bilinear')
+        x = torch.sigmoid(self.conv1(x))
+        x = x.view(x.size(0), 3, 224, 224)
+        return x
+
+
+class VAE(nn.Module):
+
+    def __init__(self, z_dim):
+        super().__init__()
+        self.encoder = ResNet18Enc(z_dim=z_dim)
+        self.decoder = ResNet18Dec(z_dim=z_dim)
+
+    def forward(self, x):
+        mean, logvar = self.encoder(x)
+        z = self.reparameterize(mean, logvar)
+        x = self.decoder(z)
+        return x, mean, logvar, z
+    
+    @staticmethod
+    def reparameterize(mean, logvar):
+        std = torch.exp(logvar / 2) # in log-space, squareroot is divide by two
+        epsilon = torch.randn_like(std)
+        return epsilon * std + mean
